1457f6ef38fa3421dbd32974b5274f4fd3d4f7e4
Michał Bluj <m.bluj@polsource.com>
2017-05-22 16:41:53 +0200

    contacts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var router = express.Router();
+
 var pg = require('pg');
-
 var app = express();
-app.set('port', (process.env.PORT || 5000));
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -11,58 +12,55 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/offers', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT name FROM offers ', function(err, result) {
-      done();
-      if (err) {
-        console.error(err); response.send("Error " + err);
-      } else {
-        response.render('pages/offers', {results: result.rows} );
-      }
-    });
-  });
-});
-
-
-app.get('/contacts', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT firstname, lastname FROM salesforce.contact ', function(err, result) {
-      done();
-      if (err) {
-        console.error(err); response.send("Error " + err);
-      } else {
-        response.render('pages/contacts', {results: result.rows} );
-      }
-    });
-  });
-});
-
-
-var router = express.Router();
 
 router.use(function(req,res,next){
     next();
 });
 
+app.get('/', function(request, response) {
+    response.render('pages/index');
+});
+
+app.get('/offers', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT name FROM offers ', function(err, result) {
+            done();
+            if (err) {
+                console.error(err); response.send("Error " + err);
+            } else {
+                response.render('pages/offers', {results: result.rows} );
+            }
+        });
+    });
+});
+
+
+app.get('/contacts', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT firstname, lastname FROM salesforce.contact ', function(err, result) {
+        done();
+        if (err) {
+            console.error(err); response.send("Error " + err);
+        } else {
+            response.render('pages/contacts', {results: result.rows} );
+        }
+        });
+    });
+});
+
 router.route('/offers').get(
     function(req, res) {
-          pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             client.query('SELECT name FROM offers ', function(err, result){
-                                                         done();
-                                                         if(err){
-                                                             res.json(err);
-                                                         } else {
-                                                             res.json(result.rows);
-                                                         }
-                                                     });
+                done();
+                if(err){
+                    res.json(err);
+                } else {
+                    res.json(result.rows);
+                }
+            });
         });
     }
 );
@@ -74,15 +72,15 @@ router.route('/offers').post(function(req, res) {
 
 router.route('/contacts').get(
     function(req, res) {
-          pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             client.query('SELECT firstname, lastname FROM salesforce.contact ', function(err, result){
-                                                         done();
-                                                         if(err){
-                                                             res.json(err);
-                                                         } else {
-                                                             res.json(result.rows);
-                                                         }
-                                                     });
+                done();
+                if(err){
+                    res.json(err);
+                } else {
+                    res.json(result.rows);
+                }
+            });
         });
     }
 );
@@ -92,6 +90,5 @@ router.route('/contacts').post(function(req, res) {
         res.json({ message: 'You have done successful contacts post call with following params : ' + req.params });
     }
 );
-
 
 app.use('/rest',router);
