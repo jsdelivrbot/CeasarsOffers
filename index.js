bf592322aed55056b48b6a382a77e20c619be00f
Michał Bluj <m.bluj@polsource.com>
2017-05-22 15:33:20 +0200

    contacts

--- index.js
+++ index.js
@@ -55,17 +55,19 @@ router.use(function(req,res,next){
 router.route('/offers').get(
     function(req, res) {
           pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query('SELECT name FROM offers ', function(err, result){
-                                                         done();
-                                                         if(err){
-                                                             res.json(err);
-                                                         } else {
-                                                             res.json(result.rows);
-                                                         }
-                                                     });
+            client.query('SELECT name FROM offers ', offersCallBack);
         });
     }
 );
+
+var offersCallBack = function(err, result){
+                                                                              done();
+                                                                              if(err){
+                                                                                  res.json(err);
+                                                                              } else {
+                                                                                  res.json(result.rows);
+                                                                              }
+                                                                          };
 
 router.route('/contacts').get(
     function(req, res) {
