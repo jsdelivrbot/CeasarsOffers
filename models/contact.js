2db63ca347eecabf72a2e2e1438b7fdf6d5d8954
Michał Bluj <m.bluj@polsource.com>
2017-05-23 16:12:45 +0200

    file generator

--- models/contact.js
+++ models/contact.js
@@ -1,7 +1,7 @@
 
 var pg = require('pg');
 
-export.getRecordsBeforeDate = function(dateParam){
+exports.getRecordsBeforeDate = function(dateParam){
     var results;
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT name FROM salesforce.contact ', function(err, result) {
