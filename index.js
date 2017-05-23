b43490dd17d5bb84820d2dbe55af94abe7884956
Michał Bluj <m.bluj@polsource.com>
2017-05-23 13:37:21 +0200

    file generator

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-var fileGenerator = app.require('./controllers/contactFileGenerator.js');
-var contactModel =  app.require('./model/contact.js');
+var fileGenerator = require('./controllers/contactFileGenerator.js');
+var contactModel =  require('./model/contact.js');
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
