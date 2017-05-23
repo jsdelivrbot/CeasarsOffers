aee4453ed4ef5b25ea3cc3737d0b7322a06a44db
Michał Bluj <m.bluj@polsource.com>
2017-05-23 16:37:31 +0200

    file generator

--- models/contact.js
+++ models/contact.js
@@ -1,20 +1,13 @@
+use strict;
 
 var pg = require('pg');
-
-var convertToPostgresDateTime = function(dateParam){
-    return dateParam.getFullYear() +'-' + ensureLeadingZeros(dateParam.getMonth()+1)+'-'+ensureLeadingZeros(dateParam.getDate())+' '+ensureLeadingZeros(dateParam.getHours())+':'+ensureLeadingZeros(dateParam.getMinutes())+':'+ensureLeadingZeros(dateParam.getSeconds());
-}
-
-var ensureLeadingZeros = function(param){
-    return param < 10 ? '0'+param : param;
-}
+var dateUtils = require('../utils/dateUtils.js');
 
 exports.getRecordsBeforeDate = function(dateParam){
     var results;
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        console.log('SELECT name FROM salesforce.contact where systemmodstamp < ' + convertToPostgresDateTime(dateParam));
-        client.query('SELECT name FROM salesforce.contact where systemmodstamp < ' + convertToPostgresDateTime(dateParam), function(err, result) {
+        client.query('SELECT name FROM salesforce.contact where systemmodstamp < ' + dateUtils.convertToPostgresDateTime(dateParam), function(err, result) {
             done();
             if(results){
                 results = result.rows;
