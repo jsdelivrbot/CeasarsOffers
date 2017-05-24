26f8676636c129c4d358b73ab74e1da35abaf08c
Michał Bluj <m.bluj@polsource.com>
2017-05-24 10:48:16 +0200

    ftp

--- models/contact.js
+++ models/contact.js
@@ -2,18 +2,24 @@ var pg = require('pg');
 var dateUtils = require('../utils/dateUtils.js');
 
 exports.getRecordsBeforeDate = function(dateParam){
-    var results;
+    var results = [];
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT name FROM salesforce.contact where systemmodstamp < \'' + dateUtils.convertToPostgresDateTime(dateParam)+'\'', function(err, result) {
+        /*client.query('SELECT name FROM salesforce.contact where systemmodstamp < \'' + dateUtils.convertToPostgresDateTime(dateParam)+'\'', function(err, result) {
             done();
             console.log('result ' + result.rows);
             results = result.rows;
             console.log('results ' + results);
+            return results;
+        });*/
+
+        const query = client.query('SELECT name FROM salesforce.contact where systemmodstamp < \'' + dateUtils.convertToPostgresDateTime(dateParam)+'\';');
+        // Stream results back one row at a time
+        query.on('row', (row) => {results.push(row);});
+        // After all data is returned, close connection and return results
+        query.on('end', () => {
+            done();
+            return res.json(results);
         });
     });
-
-    console.log('SELECT name FROM salesforce.contact where systemmodstamp < \'' + dateUtils.convertToPostgresDateTime(dateParam)+'\'');
-    console.log('results after ' + results);
-    return results;
 }
