1611370912ce2618bfcc4bc4c2ee70e02f353a91
Michał Bluj <m.bluj@polsource.com>
2017-05-24 14:12:20 +0200

    REST PUT offer

--- models/offer.js
+++ models/offer.js
@@ -3,6 +3,6 @@ exports.postOffer = function(request, response){
     var requestBody = request.body;
     var offers = JSON.parse(JSON.stringify(requestBody));
     for(var i =0; i<offers.length; i++){
-        console.log('offer name ' + offer.name);
+        console.log('offer name ' + offers[i].name);
     }
 }
