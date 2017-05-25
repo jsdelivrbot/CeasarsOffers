4c0bd2eca903457848dfcd47cd53a273fd86a1ff
Michał Bluj <m.bluj@polsource.com>
2017-05-25 11:17:41 +0200

    ftp read

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/generateFile',function(request,response){
 });
 
 app.get('/readFile',function(request,response){
-    var fileName = '1mb';//request.query.filePath;
+    var fileName = '1mb.pak';//request.query.filePath;
     fileUtility.readFileFromFTPServer(fileName);
     response.render('pages/index');
 });
