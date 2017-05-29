51779a3f285dc87a7f202ff49e60a9cdcb913202
Michał Bluj <m.bluj@polsource.com>
2017-05-29 12:23:20 +0200

    logging

--- controllers/caesarsLogger.js
+++ controllers/caesarsLogger.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 
 exports.log = function(loglevel,message,json){
 
-    var statement = 'insert into winston_logs (level,msg,meta) values ('+loglevel+','+message+','+json+')';
+    var statement = 'insert into winston_logs (level,msg,meta) values (\''+loglevel+'\',\''+message+'\',\'+json+'\')';
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query(statement,
