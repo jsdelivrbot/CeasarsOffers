3452862d7f10f306f5f024bd236d275c485ad05c
Michał Bluj <m.bluj@polsource.com>
2017-05-29 11:47:37 +0200

    logging

--- models/offer.js
+++ models/offer.js
@@ -5,7 +5,7 @@ var db = require("winston-postgre");
 winston.add(winston.transports.PostgreSQL, {
                                                "connectionString" : "nbqrxjobyivxmr:dbbecaadb1f248feddd2262f387fa51d314cb6c0e10b7fcadbb04cb32abbf40a@ec2-54-221-255-153.compute-1.amazonaws.com:5432/dcvg2h7ksmoa8r",
                                                "schema" : "public",
-                                               "table" : "AppLogs",
+                                               "table" : "winston_logs",
                                                "level" : "info"
                                            });
 
@@ -43,7 +43,7 @@ exports.postOffer = function(request, response, next){
              function(err, result){
                  done();
                  var timeDiff = new Date().getTime() - startTime;
-                 winston.log('info', '{timeDiff:'+timeDiff+'}');
+                 winston.log('info','exports get offer','{timeDiff:'+timeDiff+'}');
                  response.json(err ? err : result.rows);
              }
          );
