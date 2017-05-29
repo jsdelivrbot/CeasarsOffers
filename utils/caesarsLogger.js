6135671ab83fa4b14b77a7649d28b88be1aa86bd
Michał Bluj <m.bluj@polsource.com>
2017-05-29 13:24:54 +0200

    logging

--- utils/caesarsLogger.js
+++ utils/caesarsLogger.js
@@ -2,13 +2,13 @@ var pg = require('pg');
 
 exports.log = function(loglevel,message,json){
 
-    var statement = 'insert into AppLogs (level,msg,meta) values (\''+loglevel+'\',\''+message+'\',\''+json+'\')';
+    var statement = 'insert into AppLogs (level,msq,meta) values (\''+loglevel+'\',\''+message+'\',\''+json+'\')';
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query(statement,
             function(err, result) {
                 if(err){
-                    console.log('Loge save failed : ' + err);
+                    console.log('Log save failed : ' + err);
                 }
                 client.end();
             }
