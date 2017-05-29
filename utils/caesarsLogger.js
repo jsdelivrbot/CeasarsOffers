610e2e04f8b6bc3f7afcce4e74c175e4a6ddb82d
Michał Bluj <m.bluj@polsource.com>
2017-05-29 13:00:11 +0200

    logging

--- utils/caesarsLogger.js
+++ utils/caesarsLogger.js
@@ -1,8 +1,29 @@
 var pg = require('pg');
 
+const winston = require("winston");
+
+//require("winston-pg-native");
+require("winston-postgresql").PostgreSQL;
+
+const options = {
+  conString: process.env.DATABASE_URL,
+  tableName: 'AppLogs',
+  level: 'info'
+};
+
+
+const logger = new(winston.Logger)({
+  transports: [
+    new(winston.transports.Postgres)(options)
+  ]
+});
+
+
 exports.log = function(loglevel,message,json){
 
-    var statement = 'insert into AppLogs (level,msg,meta) values (\''+loglevel+'\',\''+message+'\',\''+json+'\')';
+    logger.log(loglevel,message,json);
+
+    /*var statement = 'insert into AppLogs (level,msg,meta) values (\''+loglevel+'\',\''+message+'\',\''+json+'\')';
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query(statement,
@@ -13,5 +34,5 @@ exports.log = function(loglevel,message,json){
                 client.end();
             }
         );
-    });
+    });*/
 }
