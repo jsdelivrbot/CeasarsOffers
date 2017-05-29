a8239ac56f5746ec0e6a85dd10cf442cf1498f6f
Michał Bluj <m.bluj@polsource.com>
2017-05-29 11:50:08 +0200

    logging

--- models/offer.js
+++ models/offer.js
@@ -3,7 +3,7 @@ var winston = require("winston");
 var db = require("winston-postgre");
 
 winston.add(winston.transports.PostgreSQL, {
-                                               "connectionString" : "nbqrxjobyivxmr:dbbecaadb1f248feddd2262f387fa51d314cb6c0e10b7fcadbb04cb32abbf40a@ec2-54-221-255-153.compute-1.amazonaws.com:5432/dcvg2h7ksmoa8r",
+                                               "connectionString" : process.env.DATABASE_URL,
                                                "schema" : "public",
                                                "table" : "winston_logs",
                                                "level" : "info"
