84522a1845ae0129039fd93afa8c9b3927a38db9
Michał Bluj <m.bluj@polsource.com>
2017-05-29 14:07:40 +0200

    logging

---
+++ test.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+
+var dateUtils = require('./utils/dateUtils.js');
+
+test('convert date to psql string', t => {
+  var d = new Date(2017, 0, 1, 0, 0, 0, 0);
+  t.is(dateUtils.convertToPostgresDateTime(d), '2017-01-01 00:00:00');
+});
+
+test('ensure leading zeros', t=>{
+   t.is(dateUtils.ensureLeadingZeros(1),"01");
+   t.is(dateUtils.ensureLeadingZeros(10),10);
+});
--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -2,6 +2,6 @@ exports.convertToPostgresDateTime = function(dateParam){
     return dateParam.getFullYear() +'-' + ensureLeadingZeros(dateParam.getMonth()+1)+'-'+ensureLeadingZeros(dateParam.getDate())+' '+ensureLeadingZeros(dateParam.getHours())+':'+ensureLeadingZeros(dateParam.getMinutes())+':'+ensureLeadingZeros(dateParam.getSeconds());
 }
 
-var ensureLeadingZeros = function(param){
+exports.ensureLeadingZeros = function(param){
     return param < 10 ? '0'+param : param;
 }
