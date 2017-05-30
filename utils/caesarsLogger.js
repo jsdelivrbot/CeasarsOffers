f4f0fe69d839fc2e00cdb9dc8b89787487880e2a
Michał Bluj <m.bluj@polsource.com>
2017-05-30 14:44:33 +0200

    ftp read

--- utils/caesarsLogger.js
+++ utils/caesarsLogger.js
@@ -1,9 +1,12 @@
 var pg = require('pg');
 
+var logKey = new Date().now();
+
 exports.log = function(level,message,json){
 
     var statement = 'insert into AppLogs (level,msq,meta) values (\''+level+'\',\''+message+'\',\''+json+'\')';
 
+    console.log('logKey ' + logKey);
     console.log(statement);
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
