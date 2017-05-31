9f0d35c9ff01be21b5218042a58c83013e0c79dc
Michał Bluj <m.bluj@polsource.com>
2017-05-31 09:49:10 +0200

    commit

--- utils/ftpUtils.js
+++ utils/ftpUtils.js
@@ -6,9 +6,9 @@ var caesarsLogger = require('./caesarsLogger.js');
 exports.saveFileOnFTPServer = function(records, fileName){
     var startTime = new Date().getTime();
     if(records){
-        var ftpClient = new jsFtp({host: "speedtest.tele2.net",port: 21,user: "anonymous",pass: "anonymous"});
-        var dataToBeSaved = fileUtils.convertToNiceFileContent(records);
-        var buffer = Buffer.from(dataToBeSaved);
+        var connectionParams = connectionParameters();
+        var ftpClient = new jsFtp(connectionParams);
+        var buffer = Buffer.from(fileUtils.convertToNiceFileContent(records));
         var filePath = 'upload/'+fileName;
         ftpClient.put(buffer, filePath, function(hadError) {
           console.log('Transferring file ' + fileName + ' into FTP server')
@@ -21,23 +21,21 @@ exports.saveFileOnFTPServer = function(records, fileName){
           }
         });
     } else {
-        var timeDiff = new Date().getTime() - startTime;
-        caesarsLogger.log('info','exports.saveFileOnFTPServer','{"timeDiff":"'+timeDiff+'"}');
+        caesarsLogger.log('info','exports.saveFileOnFTPServer','{"timeDiff":"' + new Date().getTime() - startTime + '"}');
     }
 }
 
 exports.readFileFromFTPServer = function(fileName,callback){
     var startTime = new Date().getTime();
+    var connectionParams = connectionParameters();
     var ftpClient = new jsFtp({host: "speedtest.tele2.net",port: 21,user: "anonymous",pass: "anonymous"});
-
     var fileContent = "";
-
-    console.log('Start read file from FTP server');
-
+    console.log('Start read file from FTP server ' + connectionParams);
     ftpClient.get(fileName, function(err, socket) {
         if (err) {
             return;
         } else {
+
             socket.on("data", function(d) {
                 console.log('Reading ...');
                 fileContent += d.toString();
@@ -45,16 +43,21 @@ exports.readFileFromFTPServer = function(fileName,callback){
 
             socket.on("close", function(hadErr) {
                 if (hadErr){
-                    console.error('There was an error retrieving the file.');
-                    caesarsLogger.log('error','exports.readFileFromFTPServer','{"timeDiff":"' + new Date().getTime() - startTime + '"}');
+                    console.error('There was an error retrieving the file : ' + hadErr);
+                    caesarsLogger.log('error','exports.readFileFromFTPServer : ' + hadErr,'{"timeDiff":"' + new Date().getTime() - startTime + '"}');
                 } else {
                     console.log('Reading completed');
                     caesarsLogger.log('info','exports.readFileFromFTPServer','{"timeDiff":"' + new Date().getTime() - startTime + '"}');
                     callback(fileContent);
                 }
             });
+
             socket.resume();
         }
       }
     );
+}
+
+var connectionParameters = function(){
+    return {host: "speedtest.tele2.net",port: 21,user: "anonymous",pass: "anonymous"};
 }
