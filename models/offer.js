55ce529955e9d18ca5771a1efd779d3160514b63
Michał Bluj <m.bluj@polsource.com>
2017-06-08 12:42:50 +0200

    commit

--- models/offer.js
+++ models/offer.js
@@ -7,12 +7,20 @@ var shortid = require('shortid');
 exports.getAvailableOffers = function(request,response,next){
     var requestParameters = httpUtils.parseRequestForParameters(request);
     console.log('running get Available Offers with parameters : ' + requestParameters);
+    for(var i = 0; i < requestParameters.keys().length; i++){
+            var key = requestParameters.keys()[i];
+            console.log(key + ' : ' + requestParameters[key]);
+        }
     response.json('{}');
 }
 
 exports.getOfferDetails = function(request,response,next){
     var requestParameters = httpUtils.parseRequestForParameters(request);
     console.log('running get Offer Details with parameters : ' + requestParameters);
+    for(var i = 0; i < requestParameters.keys().length; i++){
+        var key = requestParameters.keys()[i];
+        console.log(key + ' : ' + requestParameters[key]);
+    }
     response.json('{}');
 }
 
--- utils/httpUtils.js
+++ utils/httpUtils.js
@@ -1,8 +1,6 @@
 var url = require('url');
 
 exports.parseRequestForParameters = function(requestObject){
-    var params = new Object();
     var url_parts = url.parse(requestObject.url, true);
-    console.log(url_parts.query);
-    return '';
+    return url_parts;
 }
