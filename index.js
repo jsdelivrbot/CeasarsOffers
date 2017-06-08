152b8501648eaf46b5048e862ef304231d218a84
Michał Bluj <m.bluj@polsource.com>
2017-06-08 12:28:15 +0200

    commit

--- index.js
+++ index.js
@@ -96,8 +96,8 @@ router.get('/contacts',contactModel.getContacts);
 router.post('/contacts',contactModel.postContact);
 
 router.get('/availableOffers',offerModel.getAvailableOffers); // Available Offer List
-router.get('/offerDetails',offerModel.getOfferDetails); // View Offer Details
-router.put('/modifyOffer',offerModel.modifyOffer); // Offer modification
+//router.get('/offerDetails',offerModel.getOfferDetails); // View Offer Details
+//router.put('/modifyOffer',offerModel.modifyOffer); // Offer modification
 
 router.get('/offer',offerModel.getOffer);
 router.post('/offers',offerModel.postOffer);
