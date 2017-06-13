9d82f22fa6fc9ff96baa16f25d2450a86bc07134
Michał Bluj <m.bluj@polsource.com>
2017-06-13 12:58:25 +0200

    commit

--- util/httpUtils.js
+++ util/httpUtils.js
@@ -1,6 +1,6 @@
-var url = require('url');
+let url = require('url');
 
 exports.parseRequestForParameters = function(requestObject){
-    var url_parts = url.parse(requestObject.url, true);
+    let url_parts = url.parse(requestObject.url, true);
     return url_parts.query;
 }
