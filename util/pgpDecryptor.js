8c44faf96d70035eaa0530a48f269d56c0118f58
Michał Bluj <m.bluj@polsource.com>
2017-06-23 12:45:14 +0200

    commit

--- util/pgpDecryptor.js
+++ util/pgpDecryptor.js
@@ -1,10 +1,6 @@
 var openpgp = require('openpgp'); // use as CommonJS, AMD, ES6 module or via window.openpgp
-
 openpgp.initWorker({ path:'openpgp.worker.js' }) // set the relative web worker path
-
 openpgp.config.aead_protect = true // activate fast AES-GCM mode (not yet OpenPGP standard)
-
-var options, encrypted;
 
 var pubkey = '-----BEGIN PGP PUBLIC KEY BLOCK ... END PGP PUBLIC KEY BLOCK-----';
 var privkey = '-----BEGIN PGP PRIVATE KEY BLOCK ... END PGP PRIVATE KEY BLOCK-----'; //encrypted private key
@@ -13,12 +9,14 @@ var passphrase = 'secret passphrase'; //what the privKey is encrypted with
 var privKeyObj = openpgp.key.readArmored(privkey).keys[0];
 privKeyObj.decrypt(passphrase);
 
-options = {
-    message: openpgp.message.readArmored(encrypted),     // parse armored message
-    publicKeys: openpgp.key.readArmored(pubkey).keys,    // for verification (optional)
-    privateKey: privKeyObj // for decryption
-};
+var decryptData = function(encrypted){
 
-openpgp.decrypt(options).then(function(plaintext) {
-    return plaintext.data;
-});
+    var options = {
+        message: openpgp.message.readArmored(encrypted),     // parse armored message
+        privateKey: privKeyObj // for decryption
+    };
+
+    openpgp.decrypt(options).then(function(plaintext) {
+        return plaintext.data;
+    });
+}
